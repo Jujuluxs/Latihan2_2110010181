389cad1e308dfcbd124151b4ad0ef13d54b879d9
jujuluxs <sufyannor2016@gmail.com>
2023-08-03 14:18:06 +0800

    membuat class mahasiswa dan mahasiswaBeraksi

---
+++ src/Universitas/Mahasiswa.java
@@ -0,0 +1,44 @@
+package Universitas;
+public class Mahasiswa {
+    private String npm,nama,prodi;
+    
+    public String getDetail(){
+        return nama + npm + prodi;
+    }
+
+    public Mahasiswa(String npm, String nama, String prodi) {
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+        System.out.println("NPM : " + npm );
+        System.out.println("Nama : " + nama );
+        System.out.println("prodi : " + prodi );
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
+}
+    
+
---
+++ src/Universitas/MahasiswaBerjalan.java
@@ -0,0 +1,9 @@
+package Universitas;
+public class MahasiswaBerjalan {
+    public static void main(String[] args) {
+        Mahasiswa sufyan = new Mahasiswa("2110010181","Sufyan Nor","TI");
+        
+        sufyan.getDetail();
+    
+    }
+}
